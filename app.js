f3ccc8a4a728029f36a7668567bbd9dbcd91df4e
unknown <sergishramchenko@gmail.com>
2020-02-26 12:56:56 +0300

    Improve security

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,8 +13,31 @@ const app = express();
 
 // MIDDLEWARE //
 
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Try again later'
+});
+
+app.use('/api', limiter);
+
 // Body parser, reading data from the body into req.body
-app.use(express.json());
+app.use(express.json({ limit: '100kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+// app.use(hpp({ whitelist: ['price', 'duration'] }));
+app.use(hpp());
+
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES //
 app.use('/api/v1/users', userRouter);
